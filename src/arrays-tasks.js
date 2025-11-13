bea66ee26e462c06779d6360aec116ba3599d789
evgeniy zimagorov <zimagorov.evgeniy@gmail.com>
2025-11-13 15:42:58 +0300

    feat: add function to count all occurrences of a specified item in array

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -86,8 +86,10 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce((acc, currentValue) => {
+    return currentValue === item ? acc + 1 : acc;
+  }, 0);
 }
 
 /**
